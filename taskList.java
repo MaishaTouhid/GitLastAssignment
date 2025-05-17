e9a648ef9cc8dff3c7a4d20be6d2f956ad24aa88
Maisha <maishatouhid1982004@gmail.com>
2025-05-17 18:42:25 +0600

    Fix Indentation Consistency – Ensure all lines have a uniform indentation style

--- taskList.java
+++ taskList.java
@@ -1,23 +1,33 @@
 import java.util.*;
 class ListManager {
-static Scanner scanner = new Scanner(System.in);
-static List<String> itemlist = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+    static List<String> itemlist = new ArrayList<>();
+
 public static void main(String[] args){
   while(true){
-System.out.println("1: Add 2: Remove 3: Show 4: Exit");
-int choice = scanner.nextInt();
- if(choice ==1){
- System.out.print("Item Name: ");
- scanner.nextLine();
-   itemlist.add(scanner.nextLine());
-   }
-else if(choice ==2){
-System.out.print("I: ");
-int index = scanner.nextInt();
-   if(index >=0&&index<itemlist.size())itemlist.remove(index);
- }
- else if(choice==3){
-for(int j=0;j<itemlist.size();j++)
- {System.out.println(j+": "+itemlist.get(j));}
-}
-  else if(choice==4)break;}}}
+      System.out.println("1: Add 2: Remove 3: Show 4: Exit");
+      int choice = scanner.nextInt();
+
+        if(choice ==1){
+            System.out.print("Item Name: ");
+            scanner.nextLine();
+            itemlist.add(scanner.nextLine());
+
+        }else if(choice ==2){
+            System.out.print("I: ");
+            int index = scanner.nextInt();
+            if(index >=0&&index<itemlist.size()){
+                itemlist.remove(index);
+      }
+
+        }else if(choice==3){
+            for(int j=0;j<itemlist.size();j++){
+                 System.out.println(j+": "+itemlist.get(j));
+    }
+
+        }else if(choice==4){
+            break;
+          }
+        }
+      }
+    }
